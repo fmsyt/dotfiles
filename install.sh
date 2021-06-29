d5ff18b4e305707cd98d47c2bdf215fe739b3815
fmsyt <fms20yt@gmail.com>
2021-06-29 11:12:29 +0900

    made gitconfig external file

--- install.sh
+++ install.sh
@@ -34,31 +34,10 @@ do
     ln -snfv "$link" "$file_path"
 done
 
-# update git config
-# 基本設定
-git config --global color.ui true
-git config --global core.autocrlf input
-git config --global pull.ff only
-git config --global merge.ff false
-git config --global fetch.prune true
+git config --global include.path $DOTPATH/dotifles/gitconfig
 
-# エイリアス指定
-git config --global alias.st status
-git config --global alias.df diff
-git config --global alias.cm commit
-git config --global alias.br branch
-git config --global alias.ps push
-git config --global alias.pl pull
-git config --global alias.co checkout
-
-# 日本語のファイル名をエンコードしない
-git config --global core.quotepath false
-
-# 認証情報をキャッシュ
-git config --global credential.helper 'cache --timeout=86400'
 
 unset link
 unset append_text
 unset file_path
-
 
