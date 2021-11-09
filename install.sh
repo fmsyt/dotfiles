687807f92063094f4271bb93590496b9400400d7
fmsyt <fms20yt@gmail.com>
2021-11-09 20:25:36 +0900

    updated install script

--- install.sh
+++ install.sh
@@ -1,10 +1,42 @@
-#!/bin/sh
+#!/bin/bash
 
 DOTPATH=$(cd $(dirname $0); pwd)
 DEIN_INSTALL_PATH=$HOME/.cache/dein
 
-cd $DOTPATH
 
+function append_line() {
+
+    local append_text=$1
+    local file_path=$2
+
+    # ファイルの作成先にsymlinkがある
+    local link_path=$(readlink $file_path)
+    if [ $? -eq 0 ]; then
+
+        # symlinkの参照先が指定先と同一である
+        if [ "$link_path" = "$file_path" ]; then
+            unlink $link_path
+        fi
+
+        # リンクの参照先が存在しない
+        if [ ! -e $link_path ]; then
+            unlink $file_path
+        fi
+    fi
+
+    touch $file_path
+    grep -x "$append_text" $file_path
+
+    if [ $? != 0 ]; then
+        echo "$append_text" >> $file_path
+    fi
+
+    return 0
+}
+
+
+
+cd $DOTPATH
 ./dein.sh $DEIN_INSTALL_PATH
 
 
@@ -15,12 +47,7 @@ do
     append_text="source $(pwd)/$f"
     file_path="$HOME/.$f"
 
-    touch $file_path
-    grep -x "$append_text" $file_path
-
-    if [ $? != 0 ]; then
-        echo "$append_text" >> $file_path
-    fi
+    append_line "$append_text" "$file_path"
 done
 
 
@@ -40,4 +67,6 @@ git config --global include.path $DOTPATH/gitconfig
 unset link
 unset append_text
 unset file_path
+
+unset append_line
 
