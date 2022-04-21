49cd537695c8ba416df0bb37e3d3a051a0afd976
fmsyt <fms20yt@gmail.com>
2022-04-21 17:30:05 +0900

    added pwsh on wsl

--- .bash/rc/010-wsl.bash
+++ .bash/rc/010-wsl.bash
@@ -6,5 +6,6 @@ if [ -z $SSH_TTY ]; then
     alias explorer='explorer.exe'
 else
     alias cmd.exe='/mnt/c/Windows/System32/cmd.exe'
+    alias powershell.exe='/mnt/c/Program\ Files/PowerShell/7/pwsh.exe'
 fi
 
