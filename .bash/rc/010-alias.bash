27d2462439b08c5500424bb5dd629e0ed21fdaf8
fmsyt <fms20yt@gmail.com>
2022-05-15 11:15:01 +0900

    added aliases for exa instead of ls

--- .bash/rc/010-alias.bash
+++ .bash/rc/010-alias.bash
@@ -1,8 +1,16 @@
-alias ls='ls --human-readable --group-directories-first --color=auto --ignore={NTUSER*,ntuser*}'
+if type exa >/dev/null 2>&1; then
+    ls='exa --icons'
 
-alias ll='ls -lF'
-alias la='ls -AlF'
-alias l='ls -CF'
+    alias ll='exa -l --icons'
+    alias la='exa -al --icons'
+    alias l='exa --icons'
+else
+    alias ls='ls --human-readable --group-directories-first --color=auto --ignore={NTUSER*,ntuser*}'
+
+    alias ll='ls -lF'
+    alias la='ls -AlF'
+    alias l='ls -CF'
+fi
 
 # enable color support of ls and also add handy aliases
 if [ -x /usr/bin/dircolors ]; then
