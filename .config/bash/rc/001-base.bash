d0e5a7033b9e3548651980c83ca7cc3fc7d8123d
fmsyt <fms20yt@gmail.com>
2022-06-28 11:06:24 +0900

    updated condition to enable powerline-shell

--- .config/bash/rc/001-base.bash
+++ .config/bash/rc/001-base.bash
@@ -1,8 +1,8 @@
-function _update_ps1() {
+function _update_ps1_powerline() {
     PS1="$(~/.local/bin/powerline-shell $?)"
 }
 
-if [ "$TERM" != "linux" ]; then
-    PROMPT_COMMAND="_update_ps1; $PROMPT_COMMAND"
+if [ "$TERM" != "linux" -a -f ~/.local/bin/powerline-shell ]; then
+    PROMPT_COMMAND="_update_ps1_powerline; $PROMPT_COMMAND"
 fi
 
