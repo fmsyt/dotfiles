1530437cb6fe3961279eb175959327377cf18266
fmsyt <fms20yt@gmail.com>
2022-06-28 11:23:43 +0900

    powerline for fish

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -1,3 +1,10 @@
 if status is-interactive
     # Commands to run in interactive sessions can go here
 end
+
+if test -e ~/.local/bin/powerline-shell
+    function fish_prompt
+        ~/.local/bin/powerline-shell --shell bare $status
+    end
+end
+
