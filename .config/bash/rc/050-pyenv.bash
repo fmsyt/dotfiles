f41c10d7cb15a94916b863238c369c03e4c6ad58
fmsyt <fms20yt@gmail.com>
2022-08-19 14:18:52 +0900

    enable pyenv when exists in home directory

--- .config/bash/rc/050-pyenv.bash
+++ .config/bash/rc/050-pyenv.bash
@@ -1,3 +1,5 @@
-export PYENV_ROOT="$HOME/.pyenv"
-command -v pyenv >/dev/null || export PATH="$PYENV_ROOT/shims:$PATH"
-eval "$(pyenv init -)"
+if [ -f $HOME/.pyenv ]; then
+    export PYENV_ROOT="$HOME/.pyenv"
+    command -v pyenv >/dev/null || export PATH="$PYENV_ROOT/shims:$PATH"
+    eval "$(pyenv init -)"
+fi
--- .config/fish/conf.d/050-pyenv.fish
+++ .config/fish/conf.d/050-pyenv.fish
@@ -1,3 +1,6 @@
-set -Ux PYENV_ROOT $HOME/.pyenv
-set PATH $PYENV_ROOT/shims $PATH
-alias brew="env PATH=(string replace (pyenv root)/shims '' \"\$PATH\") brew"
+if test -e $HOME/.pyenv
+    set -Ux PYENV_ROOT $HOME/.pyenv
+    set PATH $PYENV_ROOT/shims $PATH
+    alias brew="env PATH=(string replace (pyenv root)/shims '' \"\$PATH\") brew"
+end
+
