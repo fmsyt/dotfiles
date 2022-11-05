6cb1e2ebd6afb18053dabe2440c23e96a0d2387b
fmsyt <fms20yt@gmail.com>
2022-11-05 21:01:19 +0900

    set ls alias with options what defined

--- .config/bash/010-alias.bash
+++ .config/bash/010-alias.bash
@@ -34,7 +34,10 @@ elif type ls >/dev/null 2>&1; then
         fi
     done
 
-    IFS=","; ls_options_confirm+="--ignore={${ls_ignores[*]//\*/\\\*}}"
+    if type ls --ignore >/dev/null 2>&1; then
+        IFS=","; ls_options_confirm+="--ignore={{ls_ignores[*]//\*/\\\*}}"
+    fi
+
     IFS=" "; alias ls="ls ${ls_options_confirm[*]}"
 
     alias ll='ls -lF'
