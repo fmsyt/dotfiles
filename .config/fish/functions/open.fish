5b9fc5255b63c7779587c4908f10f55b8df17035
fmsyt <fms20yt@gmail.com>
2023-04-14 18:12:40 +0900

    open command on WSL

--- .config/fish/functions/open.fish
+++ .config/fish/functions/open.fish
@@ -1,7 +1,22 @@
 function open
-    if [ -f /proc/sys/fs/binfmt_misc/WSLInterop ]
+    if test -f /proc/sys/fs/binfmt_misc/WSLInterop
+        and test -z $SSH_TTY
+
         set -l current_path $(pwd)
-        cmd.exe /c start $(wslpath -w "$current_path/$argv[1]") 2> /dev/null
+        set -l full_path $(wslpath -w "$current_path/$argv[1]")
+
+        if [ -f "$full_path" ]
+            cmd.exe /c start $full_path 2> /dev/null
+        else
+            cmd.exe /c explorer $full_path 2> /dev/null
+        end
+
+        if [ $status -eq 1 ]
+            return 0
+        end
+
+        return $status
+
     else
         command open $argv
     end
