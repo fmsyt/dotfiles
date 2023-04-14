8de928cb704ab35361a90b8283890c5fe55bcdb2
fmsyt <fms20yt@gmail.com>
2023-04-14 18:49:10 +0900

    explorer command in WSL on bash

--- .config/bash/010-wsl.bash
+++ .config/bash/010-wsl.bash
@@ -3,9 +3,30 @@ if [ ! -f /proc/sys/fs/binfmt_misc/WSLInterop ]; then
 fi
 
 if [ -z $SSH_TTY ]; then
-    alias explorer='explorer.exe'
-else
     alias cmd.exe='/mnt/c/Windows/System32/cmd.exe'
     alias powershell.exe='/mnt/c/Program\ Files/PowerShell/7/pwsh.exe'
 fi
 
+
+function open() {
+    if [ -f /proc/sys/fs/binfmt_misc/WSLInterop ] && [ -z $SSH_TTY ]; then
+
+        current_path=$(pwd)
+        full_path=$(wslpath -w "$current_path/$1")
+
+        if [ -f "$full_path" ]; then
+            cmd.exe /c start $full_path 2> /dev/null
+        else
+            cmd.exe /c explorer $full_path 2> /dev/null
+        fi
+
+        if [ $? -eq 1 ]; then
+            return 0
+        fi
+
+        return $?
+
+    else
+        command open $@
+    fi
+}
