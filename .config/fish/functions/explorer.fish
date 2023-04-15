152a8529e07656bca9bc6a35db1c96daa0433223
fmsyt <fms20yt@gmail.com>
2023-04-15 18:49:13 +0900

    commit

--- .config/fish/functions/explorer.fish
+++ .config/fish/functions/explorer.fish
@@ -2,7 +2,7 @@ function explorer
     if test -f /proc/sys/fs/binfmt_misc/WSLInterop
         and test -z $SSH_TTY
 
-        set -l current_path $(pwd)
+        set -l current_path (pwd)
         set -l full_path $(wslpath -w "$current_path/$argv[1]")
 
         cmd.exe /c explorer $full_path 2> /dev/null
