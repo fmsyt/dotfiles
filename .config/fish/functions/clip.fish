a7728a499ccd0495db28ddd1b76baaafd376e916
fmsyt <fms20yt@gmail.com>
2023-04-18 19:34:46 +0900

    clip

--- .config/fish/functions/clip.fish
+++ .config/fish/functions/clip.fish
@@ -1,11 +1,15 @@
 function clip
-    read input
+    read -z input
 
     if test -f /proc/sys/fs/binfmt_misc/WSLInterop
         and test -z $SSH_TTY
 
-        set i (string replace -r -a '"' '\"\"' $input)
-        powershell.exe -NoProfile -NonInteractive -NoLogo -c "echo \"$i\" | Set-Clipboard"
+        # set input (echo $input | sed -E 's/\x1B\[([0-9]{1,2}(;[0-9]{1,2})?)?[mGK]//g')
+
+        # 文字列を置換する
+        set input (echo -n $input | sed -E 's/\x1B\[([0-9]{1,2}(;[0-9]{1,2})?)?[mGK]//g' | sed 's/"/""/g' | sed "s/'/''/g" | sed 's/`/``/g' | awk 'BEGIN { ORS = "`n" } { print }')
+
+        powershell.exe -NoProfile -NonInteractive -NoLogo -c "Write-Output \"$input\" | Set-Clipboard"
 
     else
         set b64 (echo $input | base64)
