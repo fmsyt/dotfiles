94881bc2fe46198cf3b8643835d94e03151d72e7
fmsyt <fms20yt@gmail.com>
2023-05-01 22:54:10 +0900

    fish config

--- .config/fish/conf.d/001-base.fish
+++ .config/fish/conf.d/001-base.fish
@@ -1,3 +1,8 @@
 set -x PATH  $HOME/.cargo/bin $PATH
 set -gx DENO_INSTALL $HOME/.deno
 set -x PATH $DENO_INSTALL/bin $PATH
+
+if type vim >/dev/null 2>&1
+    export EDITOR=vim
+    alias svim='EDITOR=vim sudoedit'
+end
