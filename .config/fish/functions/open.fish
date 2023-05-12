f287b06e925e12563199b62c1aa074f1a1fa712b
motsuni <motsuni@yotta-lab.com>
2023-05-13 00:54:30 +0900

    open.fish

--- .config/fish/functions/open.fish
+++ .config/fish/functions/open.fish
@@ -2,32 +2,36 @@ function open
 
     # `Mac OS`の`open`コマンドについて: https://oversleptabit.com/archives/4863
     # 引数の解析: argparse:  https://qiita.com/ryotako/items/e36114ec1f86dbdd274a
-
-    argparse -n sugoi 'h/help' -- $argv
+    argparse 'h/help' 'a=' 'args=' -- $argv
     or return 1
 
     if set -lq _flag_help
-        echo "Usage: open [options] [file] [--args [...args]]"
+        echo "Usage: open [options] [file] [...args]"
         echo ""
         echo "Options:"
         echo "    -h or --help     Print this help message"
         echo "    -a <name>        Application name to open file or directory"
+        echo "    --args           Args to append on call"
 
         return
     end
 
 
+    set -lq _flag_args
 
     if test -f /proc/sys/fs/binfmt_misc/WSLInterop
         and test -z $SSH_TTY
 
-        set -l current_path (pwd)
-        set -l full_path (wslpath -w "$current_path/$argv[1]")
+        set -lq _flag_a
 
-        if [ -f "$full_path" ]
-            cmd.exe /c start $full_path 2> /dev/null
+        set -l current_path (pwd)
+        set -l full_path "$current_path/$argv[1]"
+        set -l win_path (wslpath -w $full_path)
+
+        if [ -n $_flag_a ] | [ -f "$current_path/$argv[1]" ]
+            powershell.exe -Command start $_flag_a $win_path $argv[2..-1] $_flag_args 2> /dev/null
         else
-            cmd.exe /c explorer $full_path 2> /dev/null
+            powershell.exe -Command explorer (wslpath -w $full_path) 2> /dev/null
         end
 
         if [ $status -eq 1 ]
@@ -37,6 +41,6 @@ function open
         return $status
 
     else
-        command open $argv
+        command open $argv $_flag_args
     end
 end
