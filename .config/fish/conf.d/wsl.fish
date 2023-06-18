16235ec47584b69d1f8bcdccafca05b0bd434f1b
motsuni <motsuni@yotta-lab.com>
2023-06-18 14:03:38 +0900

    :+1: use registory on wsl

---
+++ .config/fish/conf.d/wsl.fish
@@ -0,0 +1,26 @@
+if [ ! -f /proc/sys/fs/binfmt_misc/WSLInterop ]
+    return;
+end
+
+if [ -z $SSH_TTY ]
+    alias cmd.exe=(wslpath "C:\Windows\System32\cmd.exe")
+    alias powershell.exe=(wslpath "C:\Windows\System32\WindowsPowerShell\v1.0\powershell.exe")
+end
+
+
+function get_registory
+    if [ ! -f /proc/sys/fs/binfmt_misc/WSLInterop ]
+        echo "This function is run on WSL."
+        exit 1;
+    end
+
+    echo (powershell.exe -c "(New-Object -ComObject WScript.Shell).RegRead('$argv[1]')")
+end
+
+if [ -L "~/Downloads" ]
+    unlink ~/Downloads
+end
+if [ ! -e "~/Downloads" ]
+    set p (get_registory "HKCU\Software\Microsoft\Windows\CurrentVersion\Explorer\User Shell Folders\{7D83EE9B-2244-4E70-B1F5-5393042AF1E4}")
+    # ln -s (wslpath $p) ~/
+end
