732bb561e23f4ff29540db7cbd017bce736ab31d
motsuni <motsuni@yotta-rc.dev>
2024-02-03 13:53:27 +0900

    :+1: stop script if unzip command is not exitst

--- scripts/fonts/monaspace.sh
+++ scripts/fonts/monaspace.sh
@@ -1,11 +1,16 @@
 #!/bin/sh
 
+if type unzip >/dev/null 2>&1; then
+  echo "unzip command is installed"
+else
+  echo "unzip command is not installed"
+  exit 1
+fi
+
 latest_url=$(curl -w "%{redirect_url}" -s -o /dev/null https://github.com/githubnext/monaspace/releases/latest)
 # https://github.com/githubnext/monaspace/releases/tag/v1.000
 
 tag=$(echo $latest_url | awk -F '/' '{print $NF}')
-echo $tag
-
 latest_url=$(echo $latest_url | sed 's/tag/download/')
 
 file_name=monaspace-$tag.zip
