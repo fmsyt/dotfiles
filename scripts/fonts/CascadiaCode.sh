c63c707cabd2a0353864c22e486f99d5bbcecee2
motsuni <motsuni@yotta-rc.dev>
2024-02-03 13:48:03 +0900

    :+1: install path changed

--- scripts/fonts/CascadiaCode.sh
+++ scripts/fonts/CascadiaCode.sh
@@ -8,7 +8,7 @@ latest_url=$(echo $latest_url | sed 's/tag/download/')
 rm /tmp/CascadiaCode.tar.xz
 command wget -P /tmp/ $latest_url/CascadiaCode.tar.xz
 
-mkdir -p $HOME/.fonts
-command tar Jxfv /tmp/CascadiaCode.tar.xz -C $HOME/.fonts/
+mkdir -p $HOME/.fonts/CascadiaCode
+command tar Jxfv /tmp/CascadiaCode.tar.xz -C $HOME/.fonts/CascadiaCode/
 
 rm /tmp/CascadiaCode.tar.xz
