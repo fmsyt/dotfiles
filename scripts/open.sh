15bf9f040856501c4b43eb3ca17a013be8fa1a68
motsuni <motsuni@yotta-rc.dev>
2024-02-15 17:36:20 +0900

    :coffee: refactor

--- scripts/open.sh
+++ scripts/open.sh
@@ -1,14 +1,23 @@
 #!/bin/bash
 
-path="."
+if [ $(uname) = "Darwin" ]; then
+    command open $@
+    exit 0
+fi
+
 verbose=0
 
-option_only=0
+path_list=()
+
+if [ "$#" -eq 0 ]; then
+    path_list+=(".")
+fi
 
 while [ "$#" -gt 0 ]; do
     case "$1" in
         -h|--help)
-            echo "Usage: open [options] [file] [...args]"
+            echo "Usage: open [-h]"
+            echo "       open [-v] [-a <name>] [...<path|url>] [--args ...]"
             echo ""
             echo "Options:"
             echo "    -h | --help      Print this help message"
@@ -22,7 +31,6 @@ while [ "$#" -gt 0 ]; do
         -a)
             shift
             app_name="$1"
-            option_only=1
             shift
             ;;
         -v|--verbose)
@@ -35,14 +43,7 @@ while [ "$#" -gt 0 ]; do
             break
             ;;
         *)
-            if [ "$option_only" -eq 1 ]; then
-                echo "Error: Invalid option '$1'"
-                exit 1
-            fi
-
-            path="$1"
-            option_only=0
-
+            path_list+=("$1")
             shift
             ;;
     esac
@@ -55,34 +56,27 @@ if [ -n "$SSH_CONNECTION" ]; then
 fi
 
 
-if [ "$verbose" -eq 1 ]; then
-    echo "[VERBOSE] path: $path"
-    echo "[VERBOSE] app: $app_name"
-    echo "[VERBOSE] args: ${args[@]}"
-fi
-
-
-
 if [ -n "$WSL_DISTRO_NAME" ]; then
 
-    win_path=$path
+    for path in "${path_list[@]}"; do
+        win_path=$path
 
-    has_protocol=$(echo "$path" | grep -E "^[a-zA-Z]+://")
-    if [ -z "$has_protocol" ]; then
-        full_path=$(realpath "$path")
-        win_path=$(wslpath -w "$full_path")
-    fi
+        has_protocol=$(echo "$path" | grep -E "^[a-zA-Z]+://")
+        if [ -z "$has_protocol" ]; then
+            full_path=$(realpath "$path")
+            win_path=$(wslpath -w "$full_path")
+        fi
 
+        if [ -z "$app_name" ]; then
+            app_name=start
+        fi
 
-    if [ -z "$app_name" ]; then
-        app_name=start
-    fi
+        [ "$verbose" -eq 1 ] && echo "[VERBOSE] cmd.exe /u /q /c $app_name \"${win_path}\" ${args[@]}"
+        cmd.exe /u /q /c $app_name "${win_path}" ${args[@]} 2> /dev/null
+    done
 
-    [ "$verbose" -eq 1 ] && echo "[VERBOSE] cmd.exe /q /c $app_name \"${win_path}\" ${args[@]}"
-    cmd.exe /q /c $app_name "${win_path}" ${args[@]} 2> /dev/null
+    exit 0
 
 elif command -v xdg-open >/dev/null; then
     xdg-open $@
-else
-    command open $@
 fi
