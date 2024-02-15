92deb8124cc406c499a8c8a2703fc9b783bca007
motsuni <motsuni@yotta-rc.dev>
2024-02-15 16:16:06 +0900

    :+1: open script uses start command on wsl

--- scripts/open.sh
+++ scripts/open.sh
@@ -1,68 +1,69 @@
 #!/bin/bash
 
-if [ -n "$WSL_DISTRO_NAME" ] && [ -z "$SSH_TTY" ]; then
+path="."
+verbose=0
 
-    path="."
-    verbose=0
+while [ "$#" -gt 0 ]; do
+    case "$1" in
+        -h|--help)
+            echo "Usage: open [options] [file] [...args]"
+            echo ""
+            echo "Options:"
+            echo "    -h | --help      Print this help message"
+            echo "    -v | --verbose   Print verbose messages"
+            echo "    -a <name>        Application name to open file or directory"
+            echo ""
+            echo "    --args           Args to append on call"
 
-    while [ "$#" -gt 0 ]; do
-        case "$1" in
-            -h|--help)
-                echo "Usage: open [options] [file] [...args]"
-                echo ""
-                echo "Options:"
-                echo "    -h | --help      Print this help message"
-                echo "    -v | --verbose   Print verbose messages"
-                echo "    -a <name>        Application name to open file or directory"
-                echo ""
-                echo "    --args           Args to append on call"
+            exit 0
+            ;;
+        -a)
+            shift
+            app_name="$1"
+            shift
+            ;;
+        -v|--verbose)
+            verbose=1
+            shift
+            ;;
+        --args)
+            shift
+            args=("${@:1}")
+            break
+            ;;
+        *)
+            path="$1"
+            shift
+            ;;
+    esac
+done
 
-                exit 0
-                ;;
-            -a)
-                shift
-                app_name="$1"
-                shift
-                ;;
-            -v|--verbose)
-                verbose=1
-                shift
-                ;;
-            --args)
-                shift
-                args=("${@:1}")
-                break
-                ;;
-            *)
-                path="$1"
-                shift
-                ;;
-        esac
-    done
 
-    if [ "$verbose" -eq 1 ]; then
-        echo "[VERBOSE] path: $path"
-        echo "[VERBOSE] app: $app_name"
-        echo "[VERBOSE] args: ${args[@]}"
-    fi
+if [ -n "$SSH_CONNECTION" ]; then
+    echo "Error: Cannot open file or directory from SSH connection"
+    exit 1
+fi
+
+
+if [ "$verbose" -eq 1 ]; then
+    echo "[VERBOSE] path: $path"
+    echo "[VERBOSE] app: $app_name"
+    echo "[VERBOSE] args: ${args[@]}"
+fi
+
+
+
+if [ -n "$WSL_DISTRO_NAME" ]; then
 
     full_path=$(realpath "$path")
     win_path=$(wslpath -w "$full_path")
 
-    if [ -n "$app_name" ]; then
-        if [ "$verbose" -eq 1 ]; then
-            echo "[VERBOSE] powershell.exe -Command \"Invoke-Expression \\\"$app_name ${win_path} ${args[@]}\\\"\""
-        fi
-
-        powershell.exe -Command "Invoke-Expression \"$app_name ${win_path} ${args[@]}\"" 2> /dev/null
-    else
-        if [ "$verbose" -eq 1 ]; then
-            echo "[VERBOSE] powershell.exe -Command Invoke-Item \"$win_path\" \"${args[@]}\""
-        fi
-
-        powershell.exe -Command Invoke-Item "$win_path" "${args[@]}" 2> /dev/null
+    if [ -z "$app_name" ]; then
+        app_name=start
     fi
-    exit $?
+
+    [ "$verbose" -eq 1 ] && echo "[VERBOSE] cmd.exe /q /c $app_name \"${win_path}\" ${args[@]}"
+    cmd.exe /q /c $app_name "${win_path}" ${args[@]} 2> /dev/null
 
 elif command -v xdg-open >/dev/null; then
     xdg-open $@
