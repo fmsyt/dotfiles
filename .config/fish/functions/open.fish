9918de66ba93ae3b13b208e7caa965fef1ba4a5e
motsuni <motsuni@yotta-rc.dev>
2024-04-09 19:03:19 +0900

    :shower: remove unnecessary function

--- .config/fish/functions/open.fish
+++
@@ -1,9 +0,0 @@
-function open
-
-    if ! test -f $DOTFILES_DIR"/scripts/open.sh"
-        echo "open.sh not found"
-        return 1
-    end
-
-    command bash $DOTFILES_DIR"/scripts/open.sh" $argv
-end
