d94495ef537e4a904c986e74cb13aa55db443cfb
motsuni <motsuni@yotta-rc.dev>
2024-04-10 12:18:50 +0900

    symlinkに対応

--- install.sh
+++ install.sh
@@ -66,7 +66,11 @@ cpdir() {
 
         mkdir -p "$dst/$file_dir"
         if [ -L "$tmp_dir/$src_basename/$file" ]; then
-            cp -P "$tmp_dir/$src_basename/$file" "$dst/$file"
+            # 参照先の絶対パスを取得
+            orig_path=$(readlink "$tmp_dir/$src_basename/$file")
+
+            # シンボリックリンクを作成
+            ln -snf "$orig_path" "$dst/$file"
         else
             cp "$tmp_dir/$src_basename/$file" "$dst/$file"
         fi
