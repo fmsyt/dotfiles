c1977e606d46ed2224f584566b57bb2b9621de75
motsuni <motsuni@yotta-rc.dev>
2024-04-11 11:02:21 +0900

    path更新

--- .config/fish/functions/open.fish
+++ .config/fish/functions/open.fish
@@ -1,9 +1,9 @@
 function open
 
-    if ! test -f "$HOME/.config/sh/open.sh"
+    if ! test -f "$HOME/.config/sh/scripts/open.sh"
         echo "open.sh not found"
         return 1
     end
 
-    command bash $HOME/.config/sh/open.sh $argv
+    command bash $HOME/.config/sh/scripts/open.sh $argv
 end
