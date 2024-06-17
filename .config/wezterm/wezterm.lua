f722be96064aeb7da4fcff8e25fd48a617606f63
motsuni <motsuni@yotta-rc.dev>
2024-06-17 13:57:29 +0900

    wezterm theme

--- .config/wezterm/wezterm.lua
+++ .config/wezterm/wezterm.lua
@@ -1,15 +1,54 @@
 local wezterm = require 'wezterm'
 local config = wezterm.config_builder()
 
-config.color_scheme = 'Monokai Soda (Gogh)'
 config.initial_cols = 120
 config.initial_rows = 32
 config.use_ime = true
+config.color_scheme = 'Monokai Soda (Gogh)'
 
-config.font = wezterm.font_with_fallback {
-    'CaskaydiaCove Nerd Font',
-    'Cascadia Code',
+config.window_frame = {
+    font_size = 10.0
 }
+
+local bg_active = '#1a1a1a'
+local bg_inactive = '#333333'
+local bg_hover = '#222222'
+
+config.colors = {
+    tab_bar = {
+        background = bg_active,
+        active_tab = {
+            -- The color of the background area for the tab
+            bg_color = bg_active,
+            -- The color of the text for the tab
+            fg_color = '#c0c0c0',
+            intensity = 'Normal',
+            underline = 'None',
+            italic = false,
+            strikethrough = false
+        },
+        inactive_tab = {
+            bg_color = bg_inactive,
+            fg_color = '#808080'
+        },
+        inactive_tab_hover = {
+            bg_color = bg_hover,
+            fg_color = '#909090',
+            italic = true
+        },
+        new_tab = {
+            bg_color = bg_inactive,
+            fg_color = '#808080'
+        },
+        new_tab_hover = {
+            bg_color = bg_hover,
+            fg_color = '#909090',
+            italic = true
+        }
+    }
+}
+
+config.font = wezterm.font_with_fallback {'CaskaydiaCove Nerd Font', 'Cascadia Code'}
 
 -- config.keys = {{
 --     key = "\\",
@@ -20,7 +59,6 @@ config.font = wezterm.font_with_fallback {
 --         if info then
 --             -- wezterm.log_info(tostring(info.pid) .. ' ' .. info.executable)
 --         end
-
 
 --         window:perform_action(
 --             wezterm.action.SplitPane {
@@ -33,13 +71,16 @@ config.font = wezterm.font_with_fallback {
 --     end)
 -- }}
 
-config.mouse_bindings = {
-    {
-        event = { Down = { streak = 1, button = 'Right' } },
-        mods = 'NONE',
-        action = wezterm.action.PasteFrom 'Clipboard'
-    }
-}
+config.mouse_bindings = {{
+    event = {
+        Down = {
+            streak = 1,
+            button = 'Right'
+        }
+    },
+    mods = 'NONE',
+    action = wezterm.action.PasteFrom 'Clipboard'
+}}
 
 local launch_menu = {}
 
