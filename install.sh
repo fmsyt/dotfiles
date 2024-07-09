91232e4da30f7bcab9273913f2e1d48e3b36d47e
motsuni <motsuni@yotta-rc.dev>
2024-07-09 10:42:28 +0900

    refactor install scripts

--- install.sh
+++ install.sh
@@ -138,6 +138,33 @@ linkfiles() {
         syncfiles "$dotdir"
     done
 
+}
+
+post_ssh_install() {
+
+    SSH_CONFIG_DIR="$HOME/.ssh/shared"
+    SSH_INCLUDE_DIRECTIVE="Include $SSH_CONFIG_DIR/*.conf"
+    SSH_CONFIG_FILE="$HOME/.ssh/config"
+
+    if [ -f $SSH_CONFIG_FILE ]; then
+        if grep -F -q "$SSH_INCLUDE_DIRECTIVE" $SSH_CONFIG_FILE; then
+            echo "SSH config already includes shared config. Skipping..."
+        else
+            echo "Adding shared SSH config include directive..."
+
+            # Add first line to the file
+            sed -i "1s;^;$SSH_INCLUDE_DIRECTIVE\n;" $SSH_CONFIG_FILE
+
+        fi
+    else
+        echo "Creating SSH config file..."
+        echo $SSH_INCLUDE_DIRECTIVE > $SSH_CONFIG_FILE
+    fi
+}
+
+post_install() {
+    post_ssh_install
+
     git config --global include.path "$DOTFILES_DIR/.gitconfig"
 
     if [ -z "$(ls -A $BACKUP_DIR)" ]; then
@@ -147,6 +174,11 @@ linkfiles() {
     fi
 
     echo "\e[1;36mInstall completed.\e[m"
+}
+
+do_install() {
+    linkfiles
+    post_install
 }
 
 while [ $# -gt 0 ]; do
@@ -169,4 +201,4 @@ while [ $# -gt 0 ]; do
     esac
 done
 
-linkfiles
+do_install
