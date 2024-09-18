836dad75b7bf1f44b448ebcbaf61f442a25a1810
motsuni <motsuni@yotta-rc.dev>
2024-09-18 18:34:47 +0900

    fix

--- .config/bash/050-starship.bash
+++ .config/bash/050-starship.bash
@@ -1,6 +1,8 @@
-if command -v starship &>/dev/null; then
-    eval "$(starship init bash)"
+if ! command -v starship &>/dev/null; then
+    return
 fi
+
+eval "$(starship init bash)"
 
 if [ -f /etc/bash_completion ]; then
     eval "$(starship completions bash)"
