854f8c3536d81c2bdd8f6ca644ac1cacf6c88791
motsuni <motsuni@yotta-rc.dev>
2024-10-21 10:37:49 +0900

    fixed download URL of nerdfont

--- scripts/fonts/CaskaydiaCove.sh
+++ scripts/fonts/CaskaydiaCove.sh
@@ -7,10 +7,14 @@ latest_url=$(echo $latest_url | sed 's/tag/download/')
 
 tmp_dir=$(mktemp -d)
 
-command wget -P $tmp_dir/ $latest_url/CaskaydiaCove.tar.xz
+command wget -P $tmp_dir/ $latest_url/CascadiaCode.tar.xz
+if [ $? -ne 0 ]; then
+  echo "Download failed"
+  exit 1
+fi
 
 mkdir -p $HOME/.fonts/CaskaydiaCove
-command tar Jxfv $tmp_dir/CaskaydiaCove.tar.xz -C $HOME/.fonts/CaskaydiaCove/
+command tar Jxfv $tmp_dir/CascadiaCode.tar.xz -C $HOME/.fonts/CaskaydiaCove/
 
 command fc-cache -f -v
 
