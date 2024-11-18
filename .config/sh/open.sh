1f01379d5b8091cccf47636813a2880f9d1f992a
motsuni <motsuni@yotta-rc.dev>
2024-11-18 19:07:22 +0900

    refactor open.sh

--- .config/sh/open.sh
+++ .config/sh/open.sh
@@ -1,7 +1,8 @@
 #!/bin/sh
 
 open() {
-    script_path="$HOME/.config/sh/scripts/open.sh"
+    dotfiles_dir=$(dirname "$HOME/.config")
+    script_path="$dotfiles_dir/scripts/linux/utils/$1.sh"
     if [ ! -f "$script_path" ]; then
         echo "open: $script_path: No such file or directory" >&2
         return 1
