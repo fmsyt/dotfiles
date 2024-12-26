4f83a19ab9c09b24058575d8d57dc6c1ba16c08f
motsuni <motsuni@yotta-rc.dev>
2024-12-26 23:32:54 +0900

    lsの代替コマンドをexaからezaに変更

--- .config/fish/functions/la.fish
+++ .config/fish/functions/la.fish
@@ -1,18 +1,22 @@
 function la
-    # check if install exa
-    if ! command -v exa > /dev/null 2>&1
-        ls -AlF
-        return
-    end
 
     set -l ls_ignores 'ntuser.*' 'NTUSER.*' 'Thumbs.db' 'thumbs.db' 'desktop.ini'
 
     set -l ig (string join '|' $ls_ignores)
     set -l opts "-I \"$ig\""
 
-    if test "$DOTFILES_DIR/scripts/linux/utils/exa-supported-icons.sh"
-        command exa --icons -agl $opts $argv
-    else
-        command exa -agl $opts $argv
+    if command -v eza > /dev/null 2>&1
+        command eza --icons -agl $opts $argv
+        return
+
+    else if command -v exa > /dev/null 2>&1
+        if test "$DOTFILES_DIR/scripts/linux/utils/exa-supported-icons.sh"
+            command exa --icons -agl $opts $argv
+        else
+            command exa -agl $opts $argv
+        end
+        return
     end
+
+    command ls -AlF
 end
