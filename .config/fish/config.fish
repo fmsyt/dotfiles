550093fa2750d6aac131c8af19299043c8214dd2
motsuni <motsuni@yotta-rc.dev>
2025-06-23 16:14:52 +0900

    apply fish plugin information to local

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -7,6 +7,15 @@ if status is-interactive
         set_color normal
         echo "see: https://github.com/jorgebucaran/fisher"
         echo
+
+    else
+        set -l fish_config_root $HOME/.config/fish
+        if [ ! -f $fish_config_root/fish_plugins ]
+            touch $fish_config_root/fish_plugins
+        end
+
+        diff --new-line-format="%L" --old-line-format="" --unchanged-line-format="" \
+            $fish_config_root/fish_plugins $fish_config_root/fish_plugins_shared >> $fish_config_root/fish_plugins
     end
 end
 
