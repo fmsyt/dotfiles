4bd8e94b8047cdc124328950fa968918c3ce1ba5
motsuni <motsuni@yotta-rc.dev>
2025-06-23 13:30:29 +0900

    show suggestion on interactive fish

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -1,11 +1,12 @@
 if status is-interactive
     # Commands to run in interactive sessions can go here
+
+    if ! type -q fisher > /dev/null
+        set_color green
+        echo -e "Fisher is not installed. Please install it to use extensions if you want."
+        set_color normal
+        echo "see: https://github.com/jorgebucaran/fisher"
+        echo
+    end
 end
 
-if ! type -q fisher > /dev/null
-    set_color green
-    echo -e "Fisher is not installed. Please install it to use extensions if you want."
-    set_color normal
-    echo "see: https://github.com/jorgebucaran/fisher"
-    echo
-end
