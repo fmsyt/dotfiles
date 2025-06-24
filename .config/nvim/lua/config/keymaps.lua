8aa8da8b14fd3125dbab2f0c66daf0e471f1db67
h.sakamoto <h.sakamoto@protimes-souken.com>
2025-06-24 11:09:30 +0900

    lazyvim

--- .config/nvim/lua/config/keymaps.lua
+++ .config/nvim/lua/config/keymaps.lua
@@ -1,3 +1,8 @@
 -- Keymaps are automatically loaded on the VeryLazy event
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
+
+vim.api.nvim_set_keymap("n", "J", "5j", { noremap = true })
+vim.api.nvim_set_keymap("n", "K", "5k", { noremap = true })
+
+vim.api.nvim_set_keymap("n", "U", ":redo<CR>", { noremap = true })
