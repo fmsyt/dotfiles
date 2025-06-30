a65757db08c92030bda33f2066261bf38079329d
motsuni <motsuni@yotta-rc.dev>
2025-06-30 14:36:48 +0900

    remap for nvim-lsp

---
+++ .config/nvim/lua/plugins/lsp.lua
@@ -0,0 +1,20 @@
+-- since this is just an example spec, don't actually load anything here and return an empty spec
+-- stylua: ignore
+
+-- every spec file under the "plugins" directory will be loaded automatically by lazy.nvim
+--
+-- In your plugin files, you can:
+-- * add extra plugins
+-- * disable/enabled LazyVim plugins
+-- * override the configuration of LazyVim plugins
+return {
+  {
+    "neovim/nvim-lspconfig",
+    ---@class PluginLspOpts
+    opts = function()
+      local keys = require("lazyvim.plugins.lsp.keymaps").get()
+      keys[#keys + 1] = { "K", false }
+      keys[#keys + 1] = { "I", vim.lsp.buf.hover, desc = "Hover" }
+    end,
+  },
+}
