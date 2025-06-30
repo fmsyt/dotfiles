a33de319e94c0231f7b4280e784f49f9359386c1
motsuni <motsuni@yotta-rc.dev>
2025-06-30 18:35:44 +0900

    flash.nvim keymap modified

--- .config/nvim/lua/config/keymaps.lua
+++ .config/nvim/lua/config/keymaps.lua
@@ -2,7 +2,11 @@
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
 
+-- default keymaps
 vim.api.nvim_set_keymap("n", "J", "5j", { noremap = true })
 vim.api.nvim_set_keymap("n", "K", "5k", { noremap = true })
 vim.api.nvim_set_keymap("n", "U", ":redo<CR>", { noremap = true })
 vim.api.nvim_set_keymap("n", "*", "*N", { noremap = true, silent = true })
+
+-- folke/flash.nvim
+vim.keymap.del("n", "s")
