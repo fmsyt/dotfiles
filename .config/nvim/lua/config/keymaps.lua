6b0c81caf09a0a7391dc3d4b309a63bfd5e01e7a
motsuni <motsuni@yotta-rc.dev>
2025-07-08 15:07:52 +0900

    keybind on nvim

--- .config/nvim/lua/config/keymaps.lua
+++ .config/nvim/lua/config/keymaps.lua
@@ -2,28 +2,24 @@
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
 
-local ns = {
-  noremap = true,
-  silent = true,
-}
+local n = { noremap = true }
+local ns = { noremap = true, silent = true }
 
 -- common
+vim.keymap.set("n", "r", "cl", ns) -- as s
 vim.keymap.set("n", "J", "5j", ns)
 vim.keymap.set("n", "K", "5k", ns)
-vim.keymap.set("n", "U", ":redo<CR>", ns)
+vim.keymap.set("n", "U", ":redo<CR>", n)
 vim.keymap.set("n", "*", "*N", ns)
 
-vim.keymap.set("n", "<leader>q", ":q<CR>", ns)
-
--- folke/flash.nvim
-vim.keymap.del("n", "s")
+vim.keymap.set("n", "<leader>q", ":q<CR>", n)
 
 -- window
-vim.keymap.set("n", "ss", ":split<CR>", ns)
-vim.keymap.set("n", "sv", ":vsplit<CR>", ns)
-vim.keymap.set("n", "<C-\\>", ":vsplit<CR>", ns)
-vim.keymap.set("n", "sc", ":close<CR>", ns)
-vim.keymap.set("n", "gh", "<C-w>h", ns)
-vim.keymap.set("n", "gj", "<C-w>j", ns)
-vim.keymap.set("n", "gk", "<C-w>k", ns)
-vim.keymap.set("n", "gl", "<C-w>l", ns)
+vim.keymap.set("n", "ss", ":split<CR>", n)
+vim.keymap.set("n", "sv", ":vsplit<CR>", n)
+vim.keymap.set("n", "<C-\\>", ":vsplit<CR>", n)
+vim.keymap.set("n", "sc", ":close<CR>", n)
+vim.keymap.set("n", "sh", "<C-w>h", ns)
+vim.keymap.set("n", "sj", "<C-w>j", ns)
+vim.keymap.set("n", "sk", "<C-w>k", ns)
+vim.keymap.set("n", "sl", "<C-w>l", ns)
