4cd8531bbcd24c5c57477e4b8c82623d5e76a5e6
motsuni <motsuni@yotta-rc.dev>
2025-10-10 17:22:08 +0900

    colorscheme on nvim

--- .config/nvim/lua/plugins/colorscheme.lua
+++ .config/nvim/lua/plugins/colorscheme.lua
@@ -1,10 +1,26 @@
 return {
   { "savq/melange-nvim" },
-  { "craftzdog/solarized-osaka.nvim" },
+  {
+    "craftzdog/solarized-osaka.nvim",
+    config = function()
+      require("solarized-osaka").setup({
+        transparent = true,
+      })
+    end,
+  },
+  {
+    "ribru17/bamboo.nvim",
+    config = function()
+      require("bamboo").setup({
+        transparent = true,
+      })
+    end,
+  },
   {
     "LazyVim/LazyVim",
     opts = {
-      colorscheme = "solarized-osaka",
+      -- colorscheme = "solarized-osaka",
+      colorscheme = "bamboo",
     },
   },
 }
