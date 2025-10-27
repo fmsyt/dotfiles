b8a46961ece3e96f5bf7bbea1e36c1685aea2d28
motsuni <motsuni@yotta-rc.dev>
2025-10-27 17:03:09 +0900

    wsl

--- .config/fish/conf.d/wsl.fish
+++ .config/fish/conf.d/wsl.fish
@@ -1,34 +1,34 @@
 if [ ! -f /proc/sys/fs/binfmt_misc/WSLInterop ]
-    exit;
+    exit
 end
 
 if [ -z $SSH_TTY ]
-    alias cmd.exe=(wslpath "C:\Windows\System32\cmd.exe")
-    alias powershell.exe=(wslpath "C:\Windows\System32\WindowsPowerShell\v1.0\powershell.exe")
-    alias pwsh.exe=(wslpath "C:\Users\motsuni\AppData\Local\Microsoft\WindowsApps\pwsh.exe")
+    alias cmd.exe=(wslpath "C:\\Windows\\System32\\cmd.exe")
+    alias powershell.exe=(wslpath "C:\\Windows\\System32\\WindowsPowerShell\\v1.0\\powershell.exe")
+    alias pwsh.exe=(wslpath "C:\\Users\\motsuni\\AppData\\Local\\Microsoft\\WindowsApps\\pwsh.exe")
 end
 
 
 function get_registory
     if [ ! -f /proc/sys/fs/binfmt_misc/WSLInterop ]
         echo "This function is run on WSL."
-        exit 1;
+        exit 1
     end
 
-    echo (powershell.exe -c "(New-Object -ComObject WScript.Shell).RegRead('$argv[1]')" | sed s/\r//g)
+    echo (powershell.exe -c "(New-Object -ComObject WScript.Shell).RegRead('$argv[1]')" | sed s/\\r//g)
 end
 
 if [ -e "~/Desktop" ]
-    set p (get_registory "HKCU\Software\Microsoft\Windows\CurrentVersion\Explorer\User Shell Folders\Desktop")
+    set p (get_registory "HKCU\\Software\\Microsoft\\Windows\\CurrentVersion\\Explorer\\User Shell Folders\\Desktop")
     ln -s (wslpath $p) ~/
 end
 
 if [ -e "~/Documents" ]
-    set p (get_registory "HKCU\Software\Microsoft\Windows\CurrentVersion\Explorer\User Shell Folders\{F42EE2D3-909F-4907-8871-4C22FC0BF756}")
+    set p (get_registory "HKCU\\Software\\Microsoft\\Windows\\CurrentVersion\\Explorer\\User Shell Folders\\{F42EE2D3-909F-4907-8871-4C22FC0BF756}")
     ln -s (wslpath $p) ~/
 end
 
 if [ -e "~/Downloads" ]
-    set p (get_registory "HKCU\Software\Microsoft\Windows\CurrentVersion\Explorer\User Shell Folders\{7D83EE9B-2244-4E70-B1F5-5393042AF1E4}")
+    set p (get_registory "HKCU\\Software\\Microsoft\\Windows\\CurrentVersion\\Explorer\\User Shell Folders\\{7D83EE9B-2244-4E70-B1F5-5393042AF1E4}")
     ln -s (wslpath $p) ~/
 end
