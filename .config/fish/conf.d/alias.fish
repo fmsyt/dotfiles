7630a478c94362c3b133b44b7fece965ec7295e0
motsuni <motsuni@yotta-rc.dev>
2025-10-28 18:27:08 +0900

    batコマンドがないときの動作を修正

--- .config/fish/conf.d/alias.fish
+++ .config/fish/conf.d/alias.fish
@@ -1,22 +1,27 @@
 alias sudo='sudo -E'
 alias ip='ip -color'
 
+set -l use_bat 0
 set -l bat_version 0
 set -l batcat_version 0
 
 # 各桁を100倍して比較する
 if bat --version >/dev/null 2>&1 # bat 0.26.0
     set bat_version (bat --version | awk '{print $2}' | awk -F. '{printf("%d%03d%03d\n", $1, $2, $3)}')
+    set use_bat 1
 end
 
 if batcat --version >/dev/null 2>&1 # bat 0.24.0
     set batcat_version (batcat --version | awk '{print $2}' | awk -F. '{printf("%d%03d%03d\n", $1, $2, $3)}')
+    set use_bat 1
 end
 
-if test $bat_version -ge $batcat_version
-    alias cat='bat'
-else
-    alias cat='batcat'
+if test $use_bat -eq 1
+    if test $bat_version -ge $batcat_version
+        alias cat='bat'
+    else
+        alias cat='batcat'
+    end
 end
 
 abbr g git
