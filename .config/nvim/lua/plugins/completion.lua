641b453283ed0b8054687aa4fe092af46aa8c63d
motsuni <motsuni@yotta-rc.dev>
2025-10-31 18:45:54 +0900

    completion on nvim

---
+++ .config/nvim/lua/plugins/completion.lua
@@ -0,0 +1,16 @@
+return {
+  "saghen/blink.cmp",
+  opts = {
+    completion = {
+      list = {
+        selection = {
+          auto_insert = false,
+        },
+      },
+    },
+    keymap = {
+      -- https://cmp.saghen.dev/configuration/keymap.html#presets
+      preset = "enter",
+    },
+  },
+}
