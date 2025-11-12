7e3261a5d20e6e7fa181f3e838cf9f0016f81db7
motsuni <motsuni@yotta-rc.dev>
2025-11-12 10:16:05 +0900

    <S-CR> to complete suggestion on nvim

--- .config/nvim/lua/plugins/completion.lua
+++ .config/nvim/lua/plugins/completion.lua
@@ -10,7 +10,23 @@ return {
     },
     keymap = {
       -- https://cmp.saghen.dev/configuration/keymap.html#presets
-      preset = "enter",
+      -- preset = "enter",
+      ["<C-space>"] = { "show", "show_documentation", "hide_documentation" },
+      ["<C-e>"] = { "hide", "fallback" },
+      ["<S-CR>"] = { "accept", "fallback" },
+
+      ["<Tab>"] = { "snippet_forward", "fallback" },
+      ["<S-Tab>"] = { "snippet_backward", "fallback" },
+
+      ["<Up>"] = { "select_prev", "fallback" },
+      ["<Down>"] = { "select_next", "fallback" },
+      ["<C-p>"] = { "select_prev", "fallback_to_mappings" },
+      ["<C-n>"] = { "select_next", "fallback_to_mappings" },
+
+      ["<C-b>"] = { "scroll_documentation_up", "fallback" },
+      ["<C-f>"] = { "scroll_documentation_down", "fallback" },
+
+      ["<C-k>"] = { "show_signature", "hide_signature", "fallback" },
     },
   },
 }
