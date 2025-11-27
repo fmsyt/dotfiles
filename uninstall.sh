46af93c7234141b349b3b490aa5f5b3e45268820
motsuni <motsuni@yotta-rc.dev>
2025-11-27 11:21:06 +0900

    uninstall.sh

--- uninstall.sh
+++ uninstall.sh
@@ -1,54 +1,36 @@
 #!/bin/sh
 
 DOTFILES_DIR="$(cd "$(dirname "${0}")" && pwd -P)"
-BACKUP_DIR="$HOME/.dotbackup"
 
 revert() {
 
-    dotdir="$1"
-    dotname=$(basename "$dotdir")
+  dotdir="$1"
+  dotname=$(basename "$dotdir")
 
-    case "$dotname" in
-        .git*) continue ;;
-    esac
+  # is not symbolic link
+  if [ ! -L "$HOME/$dotname" ]; then
+    return
+  fi
 
-    # is not symbolic link
-    if [ ! -L "$HOME/$dotname" ]; then
-        return
-    fi
-
-    unlink "$HOME/$dotname"
-    cp -r "$DOTFILES_DIR/$dotname" "$HOME/$dotname"
+  unlink "$HOME/$dotname"
+  cp -r "$DOTFILES_DIR/$dotname" "$HOME/$dotname"
 }
-
 
 main() {
 
-    for dotdir in $DOTFILES_DIR/.??*; do
+  # for dotdir in $DOTFILES_DIR/.??*; do
+  find "$DOTFILES_DIR" -maxdepth 1 -mindepth 1 -name ".*" | while read -r dotdir; do
+    dotname=$(basename "$dotdir")
+    case "$dotname" in
+    .git*) continue ;;
+    esac
 
-        dotname=$(basename "$dotdir")
-        case "$dotname" in
-            .git*) continue ;;
-        esac
+    revert "$dotdir"
+  done
 
-        revert "$dotdir"
-    done
+  git config --global --unset include.path
 
-    for dotdir in $DOTFILES_DIR/secrets/.??*; do
-
-        dotname=$(basename "$dotdir")
-        case "$dotname" in
-            .git*) continue ;;
-        esac
-
-        revert "$dotdir"
-    done
-
-    git config --global --unset include.path
-
-    # 管理対象外のファイルを削除
-    git ls-files --other --ignored --exclude-standard | xargs rm -rf
-    echo "\e[1;36mUninstall completed.\e[m"
+  printf "\e[1;36mUninstall completed.\e[m"
 }
 
 main
