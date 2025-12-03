90c9c087012527161e646c2866b39c6c2177e864
motsuni <motsuni@yotta-rc.dev>
2025-12-03 17:10:25 +0900

    keymap on neovim

--- .config/nvim/lua/config/keymaps.lua
+++ .config/nvim/lua/config/keymaps.lua
@@ -30,3 +30,7 @@ vim.keymap.set("i", "<ScrollWheelUp>", "<C-O>2<C-Y>", ns)
 vim.keymap.set("i", "<ScrollWheelDown>", "<C-O>2<C-E>", ns)
 vim.keymap.set("v", "<ScrollWheelUp>", "2<C-Y>", ns)
 vim.keymap.set("v", "<ScrollWheelDown>", "2<C-E>", ns)
+
+if vim.env.SSH_CLIENT then
+  vim.keymap.set("v", "<leader> y", '"+y', ns)
+end
