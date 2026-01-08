4387f2dbe2ef14dbe656bb1cef72317c21b7a3b0
motsuni <motsuni@yotta-rc.dev>
2026-01-08 17:45:36 +0900

    任意のホストからのSSH接続時の動作調整

--- .config/nvim/lua/config/options.lua
+++ .config/nvim/lua/config/options.lua
@@ -4,3 +4,10 @@
 
 -- vim.g.lazyvim_php_lsp = "phpactor"
 vim.g.lazyvim_php_lsp = "intelephense"
+
+-- ./local.lua があれば読み込み
+
+local has_local = pcall(require, "config.local")
+if has_local then
+  require("config.local")
+end
--- .config/nvim/lua/plugins/ui.lua
+++ .config/nvim/lua/plugins/ui.lua
@@ -9,6 +9,22 @@ return {
   {
     "snacks.nvim",
     opts = function(_, opts)
+      local ssh_client = os.getenv("SSH_CLIENT")
+      if not ssh_client then
+        opts.scroll.enabled = true
+        return
+      end
+
+      local disable_hosts = vim.g.snacks_disable_scroll_hosts or {}
+      local ssh_host = vim.split(ssh_client, " ")[1]
+
+      for _, host in ipairs(disable_hosts) do
+        if ssh_host == host then
+          opts.scroll.enabled = false
+          return
+        end
+      end
+
       opts.scroll.enabled = true
     end,
   },
