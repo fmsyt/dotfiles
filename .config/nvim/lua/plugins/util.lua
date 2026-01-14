591633af629a3f9ccd728349bab952f9cccc2ee7
motsuni <motsuni@yotta-rc.dev>
2026-01-15 00:37:46 +0900

    oil.nvim

--- .config/nvim/lua/plugins/util.lua
+++ .config/nvim/lua/plugins/util.lua
@@ -9,4 +9,12 @@ return {
       })
     end,
   },
+  {
+    "stevearc/oil.nvim",
+    config = function()
+      require("oil").setup({
+        default_file_explorer = true,
+      })
+    end,
+  },
 }
