764af2951ceb00567c572bf9a17743681d062355
motsuni <motsuni@yotta-rc.dev>
2026-02-24 15:03:21 +0900

    imgcat

--- .config/fish/conf.d/wezterm.fish
+++ .config/fish/conf.d/wezterm.fish
@@ -2,4 +2,9 @@ if not command -v wezterm >/dev/null 2>&1
     exit
 end
 
+if set -q WEZTERM_UNIX_SOCKET
+    alias imgcat 'wezterm imgcat'
+    abbr icat imgcat
+end
+
 command wezterm shell-completion --shell fish | source
