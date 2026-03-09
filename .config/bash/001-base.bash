dca5f45ff3188209f34af277ee62c76277cec3b4
motsuni <motsuni@yotta-rc.dev>
2026-03-09 18:44:20 +0900

    bash

--- .config/bash/001-base.bash
+++ .config/bash/001-base.bash
@@ -5,12 +5,18 @@ for filename in $HOME/.config/sh/*.sh; do
 done
 
 function _update_ps1_powerline() {
-    PS1="$(~/.local/bin/powerline-shell $?)"
+  PS1="$(~/.local/bin/powerline-shell $?)"
 }
 
 export GPG_TTY=$(tty)
 
 if [ "$TERM" != "linux" -a -f ~/.local/bin/powerline-shell ]; then
-    PROMPT_COMMAND="_update_ps1_powerline; $PROMPT_COMMAND"
+  PROMPT_COMMAND="_update_ps1_powerline; $PROMPT_COMMAND"
 fi
 
+export AQUA_ROOT_DIR="${XDG_DATA_HOME:-$HOME/.local/share}/aquaproj-aqua"
+export AQUA_GLOBAL_CONFIG="$HOME/.config/aquaproj-aqua/aqua.yaml"
+
+if conmand -v aqua >/dev/null 2>&1; then
+  source <(aqua completion bash)
+fi
