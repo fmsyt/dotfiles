b85eb3615691579ac9543844aebe4952fffd52a4
motsuni <motsuni@yotta-rc.dev>
2026-06-09 13:09:36 +0900

    lazygit

--- .config/fish/functions/lazygit.fish
+++ .config/fish/functions/lazygit.fish
@@ -2,7 +2,6 @@ function lazygit --wrap lazygit
     set -l LG_CONFIG_FILE "$HOME/.config/lazygit/config.yml"
 
     if command -v delta >/dev/null
-        echo "delta found, adding delta config to lazygit"
         set LG_CONFIG_FILE "$HOME/.config/lazygit/config.yml,$HOME/.config/lazygit/config.delta.yml"
     end
 
