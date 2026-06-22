4e101b5b01616da970151e6514e2a474ba3dad23
motsuni <motsuni@yotta-rc.dev>
2026-06-22 16:16:45 +0900

    neo-tree to oil.nvim

--- .config/nvim/lua/plugins/util.lua
+++ .config/nvim/lua/plugins/util.lua
@@ -20,9 +20,13 @@ return {
 
     ---@param opts oil.SetupOpts
     opts = function(_, opts)
-      vim.keymap.set("n", "<Leader>r", function()
-        return require("oil").toggle_float(vim.fn.expand("%:p:h"))
-      end, { desc = "Open parent directory in Oil" })
+      vim.keymap.set("n", "<Leader>e", function()
+        return require("oil").toggle_float(vim.fn.expand("%:p:h"), {
+          preview = {
+            horizontal = true,
+          },
+        })
+      end, { desc = "Explorer Oil" })
 
       opts.view_options = opts.view_options or {}
       opts.view_options.show_hidden = true
