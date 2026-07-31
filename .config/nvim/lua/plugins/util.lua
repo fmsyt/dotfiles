22d66b5af65834f4207e805b111b760d666f0437
motsuni <motsuni@yotta-rc.dev>
2026-07-31 17:48:39 +0900

    change markdown extensions

--- .config/nvim/lua/plugins/util.lua
+++ .config/nvim/lua/plugins/util.lua
@@ -1,4 +1,4 @@
-return {
+local utils = {
   {
     "nvim-telescope/telescope.nvim",
     config = function()
@@ -47,7 +47,7 @@ return {
     end,
   },
   {
-    "barrettruth/live-server.nvim",
+    "selimacerbas/live-server.nvim",
   },
   {
     "nvim-neo-tree/neo-tree.nvim",
@@ -72,3 +72,59 @@ return {
     lazy = false,
   },
 }
+
+local markdown = {
+  {
+    "selimacerbas/markdown-preview.nvim",
+    dependencies = { "selimacerbas/live-server.nvim" },
+    cmd = { "MarkdownPreviewRefresh", "MarkdownPreview", "MarkdownPreviewStop" },
+    keys = {
+      {
+        "<leader>cp",
+        ft = "markdown",
+        "<cmd>MarkdownPreview<cr>",
+        desc = "Markdown Preview",
+      },
+    },
+    config = function()
+      require("markdown_preview").setup({
+        -- all optional; sane defaults shown
+        instance_mode = "takeover", -- "takeover" (one tab) or "multi" (tab per instance)
+        port = 0, -- 0 = auto (8421 for takeover, OS-assigned for multi)
+        open_browser = true,
+        default_theme = "dark", -- "dark" or "light"; initial preview theme
+        debounce_ms = 300,
+      })
+
+      vim.cmd([[do FileType]])
+    end,
+  },
+  {
+    "MeanderingProgrammer/render-markdown.nvim",
+    opts = {
+      code = {
+        sign = false,
+        width = "block",
+        right_pad = 1,
+      },
+      heading = {
+        sign = false,
+        icons = {},
+      },
+      checkbox = {
+        enabled = false,
+      },
+    },
+    ft = { "markdown", "norg", "rmd", "org", "codecompanion" },
+    config = function(_, opts)
+      require("render-markdown").setup(opts)
+      Snacks.toggle({
+        name = "Render Markdown",
+        get = require("render-markdown").get,
+        set = require("render-markdown").set,
+      }):map("<leader>um")
+    end,
+  },
+}
+
+return vim.list_extend(utils, markdown)
