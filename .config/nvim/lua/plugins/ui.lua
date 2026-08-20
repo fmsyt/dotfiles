3c2fd96c6d411c717b4bbe4d9f4b744f67220448
motsuni <motsuni@yotta-rc.dev>
2026-08-20 16:27:29 +0900

    直前のコミットと編集中のファイルの差分を表示

--- .config/nvim/lua/plugins/ui.lua
+++ .config/nvim/lua/plugins/ui.lua
@@ -20,5 +20,37 @@ return {
       local utils = require("utils")
       opts.scroll.enabled = utils.animation_disabled() ~= true
     end,
+    keys = {
+      {
+        "<leader>gd",
+        function()
+          local current_buffer_filename = vim.fn.fnamemodify(vim.api.nvim_buf_get_name(0), ":p")
+
+          require("snacks.picker").git_diff({
+            base = "HEAD^",
+            exclude = { "pnpm-lock.yaml", "package-lock.json", "*.lock" },
+
+            ---@param a snacks.picker.Item
+            ---@param b snacks.picker.Item
+            ---@return boolean
+            sort = function(a, b)
+              -- 表示中のバッファが含まれる場合は、先頭に表示する
+
+              local f1 = vim.fn.fnamemodify(a.file, ":p")
+              local f2 = vim.fn.fnamemodify(b.file, ":p")
+
+              if f1 == current_buffer_filename then
+                return true
+              elseif f2 == current_buffer_filename then
+                return false
+              end
+
+              return a.idx < b.idx
+            end,
+          })
+        end,
+        desc = "Git Diff Modified",
+      },
+    },
   },
 }
